fc8b6cf1bad7e142c9ee0fc9e5b5c13be816457a
crystal512 <crystal.a512@gmail.com>
2024-07-09 21:22:24 -0400

    Initial selenium project

---
+++ src/test/java/tek/intro/Activity1.java
@@ -0,0 +1,13 @@
+package tek.intro;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Activity1 {
+
+    public static void main(String[] args) {
+
+        ChromeDriver chromeDriver = new ChromeDriver();
+
+        chromeDriver.get("https://facebook.com");
+    }
+}
