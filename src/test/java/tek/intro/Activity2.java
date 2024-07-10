c85bb3b089088afb8496e914b01e5c41d7b79007
crystal512 <crystal.a512@gmail.com>
2024-07-09 21:41:56 -0400

    Activity 2

---
+++ src/test/java/tek/intro/Activity2.java
@@ -0,0 +1,19 @@
+package tek.intro;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Activity2 {
+
+    public static void main(String[] args) {
+
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://dev.insurance.tekschool-students.com/");
+
+        driver.manage().window().maximize();
+
+        driver.getTitle();
+
+        driver.quit();
+    }
+}
--- src/test/java/tek/intro/HandleBrowser.java
+++ src/test/java/tek/intro/HandleBrowser.java
@@ -6,8 +6,19 @@ public class HandleBrowser {
 
     public static void main(String[] args) {
 
+        //Open Chrome Browser
         ChromeDriver chromeDriver = new ChromeDriver();
 
+        //Use this method to maximize web browser
+        chromeDriver.manage().window().maximize();
+
+        //navigate to a URL
         chromeDriver.get("https://google.com");
+
+        String title = chromeDriver.getTitle();
+         System.out.println(title);
+
+        //to close browser entirely
+        chromeDriver.quit();
     }
 }
