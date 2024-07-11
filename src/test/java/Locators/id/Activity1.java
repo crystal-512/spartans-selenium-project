f3da34b4955e5867fd91fd3fc50817e4a0293a5d
crystal512 <crystal.a512@gmail.com>
2024-07-10 22:18:07 -0400

    Activity 1 Locators

---
+++ src/test/java/Locators/id/Activity1.java
@@ -0,0 +1,35 @@
+package Locators.id;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Activity1 {
+
+    public static void main(String[] args) throws InterruptedException{
+
+        WebDriver driver = new ChromeDriver();
+        Thread.sleep(3000);
+        driver.manage().window().maximize();
+        driver.get("https://retail.tekschool-students.com/");
+
+        By signInLocator = By.id("signinLink");
+        WebElement signInElement = driver.findElement(signInLocator);
+        signInElement.click();
+
+        By emailInputLocator = By.id("email");
+        WebElement emailInputElement = driver.findElement(emailInputLocator);
+        emailInputElement.sendKeys("agml@gmail.com");
+
+        By passwordLocator = By.id("password");
+        WebElement passwordElement = driver.findElement(passwordLocator);
+        passwordElement.sendKeys("123456abcd");
+
+        By loginBtnLocator = By.id("loginBtn");
+        WebElement loginBtnElement = driver.findElement(loginBtnLocator);
+        loginBtnElement.click();
+
+
+    }
+}
