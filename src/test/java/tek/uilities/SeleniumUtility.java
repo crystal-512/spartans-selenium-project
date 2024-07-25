0ec4cb76b88059cc387d01546d5295d30501cff7
crystal512 <crystal.a512@gmail.com>
2024-07-24 21:45:55 -0400

    using utility obj

--- src/test/java/tek/uilities/SeleniumUtility.java
+++ src/test/java/tek/uilities/SeleniumUtility.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class SeleniumUtility {
 
@@ -30,5 +31,9 @@ public class SeleniumUtility {
     public String getElementText(By locator) {
         return wait.until(ExpectedConditions.visibilityOfElementLocated(locator))
                 .getText();
+    }
+    public List<WebElement> getElements(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
+
     }
 }
